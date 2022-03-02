354e8cdc371da10e47e6373d6419a337885c9b64
Kai Bonsol <kaibonsol@gmail.com>
2022-03-02 09:40:34 -0600

    uhh

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
-      return cache.add('minimal.html');
+      return cache.add('pages/minimal.html');
     })
   );
 });
